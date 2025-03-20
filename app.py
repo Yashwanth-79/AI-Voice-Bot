520e04dd5185d8cdacd97a3078bca7514cb5eef1
Yashwanth M S <134080803+Yashwanth-79@users.noreply.github.com>
2025-03-20 23:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -302,12 +302,12 @@ with col2:
     st.session_state.language = languages[selected_language_name]["code"]
 
     # Use columns for Start/Stop buttons
-    col_rec = st.columns([1])
-    with col_rec:
-        if st.button("🎤 Start Recording",disabled=st.session_state.recording):
-            st.session_state.recording = True
-            st.session_state.audio_data = None  # Clear previous data
-            st.rerun()
+    
+    with st.button("🎤 Start Recording",disabled=st.session_state.recording):
+        st.session_state.recording = True
+        st.session_state.audio_data = None  # Clear previous data
+        st.rerun()
+        
 
     # with col_stop:
     #     if st.button("🛑 Stop Recording", disabled=not st.session_state.recording):
